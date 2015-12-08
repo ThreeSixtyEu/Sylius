dc08793ab0015d0926789f0c9aa638def03ab531
Lukas Mencl <l.mencl@3sixty.eu>
2015-12-08 16:55:29 +0100

    added forms processing

--- src/Sylius/Bundle/CoreBundle/Checkout/Step/PaymentShippingStep.php
+++ src/Sylius/Bundle/CoreBundle/Checkout/Step/PaymentShippingStep.php
@@ -12,6 +12,7 @@
 namespace Sylius\Bundle\CoreBundle\Checkout\Step;
 
 use Sylius\Bundle\FlowBundle\Process\Context\ProcessContextInterface;
+use Sylius\Component\Addressing\Model\ZoneInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\UserInterface;
 use Sylius\Component\Core\SyliusCheckoutEvents;
@@ -25,105 +26,115 @@ use Symfony\Component\Form\FormInterface;
  */
 class PaymentShippingStep extends CheckoutStep
 {
-    /**
-     * @var null|ZoneInterface
-     */
-    private $zones;
+	/**
+	 * @var null|ZoneInterface[]
+	 */
+	private $zones;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function displayAction(ProcessContextInterface $context)
-    {
-        $order = $this->getCurrentCart();
-        $this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_SHIPPING_INITIALIZE, $order);
+	/**
+	 * {@inheritdoc}
+	 */
+	public function displayAction(ProcessContextInterface $context)
+	{
+		$order = $this->getCurrentCart();
+		$this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_SHIPPING_INITIALIZE, $order);
 
-//        if (empty($this->zones)) {
-//            return $this->proceed($context->getPreviousStep()->getName());
-//        }
+		$this->dispatchCheckoutEvent(SyliusCheckoutEvents::ADDRESSING_INITIALIZE, $order);
+		$formAddressing = $this->createCheckoutAddressingForm($order, $this->getUser());
 
-        $this->dispatchCheckoutEvent(SyliusCheckoutEvents::ADDRESSING_INITIALIZE, $order);
-        $formAddressing = $this->createCheckoutAddressingForm($order, $this->getUser());
+		$this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_INITIALIZE, $order);
+		$formPayment = $this->createCheckoutPaymentForm($order);
 
-        $this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_INITIALIZE, $order);
-        $formPayment = $this->createCheckoutPaymentForm($order);
+		if ($order->getShippingAddress()) {
+			$this->dispatchCheckoutEvent(SyliusCheckoutEvents::SHIPPING_INITIALIZE, $order);
+			$formShipping = $this->createCheckoutShippingForm($order);
+		} else {
+			$formShipping = null;
+		}
 
-        $this->dispatchCheckoutEvent(SyliusCheckoutEvents::SHIPPING_INITIALIZE, $order);
-        $formShipping = $this->createCheckoutShippingForm($order);
+		return $this->renderStep($context, $order, $formAddressing, $formPayment, $formShipping);
+	}
 
-        return $this->renderStep($context, $order, $formAddressing, $formShipping,$formPayment);
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function forwardAction(ProcessContextInterface $context)
+	{
+		$request = $this->getRequest();
 
-    /**
-     * {@inheritdoc}
-     */
-    public function forwardAction(ProcessContextInterface $context)
-    {
-        $request = $this->getRequest();
+		$order = $this->getCurrentCart();
+		$this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_SHIPPING_INITIALIZE, $order);
 
-        $order = $this->getCurrentCart();
-        $this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_SHIPPING_INITIALIZE, $order);
+		$this->dispatchCheckoutEvent(SyliusCheckoutEvents::ADDRESSING_INITIALIZE, $order);
+		$formAddressing = $this->createCheckoutAddressingForm($order, $this->getUser());
+		$formAddressing->handleRequest($request);
 
-        $this->dispatchCheckoutEvent(SyliusCheckoutEvents::ADDRESSING_INITIALIZE, $order);
-        $formAddressing = $this->createCheckoutAddressingForm($order, $this->getUser());
+		$this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_INITIALIZE, $order);
+		$formPayment = $this->createCheckoutPaymentForm($order);
+		$formPayment->handleRequest($request);
 
-        $this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_INITIALIZE, $order);
-        $formPayment = $this->createCheckoutPaymentForm($order);
+		if ($order->getShippingAddress()) {
+			$this->dispatchCheckoutEvent(SyliusCheckoutEvents::SHIPPING_INITIALIZE, $order);
+			$formShipping = $this->createCheckoutShippingForm($order);
+			$formShipping->handleRequest($request);
+		} else {
+			$formShipping = null;
+		}
 
-        $this->dispatchCheckoutEvent(SyliusCheckoutEvents::SHIPPING_INITIALIZE, $order);
-        $formShipping = $this->createCheckoutShippingForm($order);
+		if ($formAddressing->isValid() && $formPayment->isValid() && $formShipping->isValid()) {
+			$this->dispatchCheckoutEvent(SyliusCheckoutEvents::ADDRESSING_PRE_COMPLETE, $order);
+			$this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_PRE_COMPLETE, $order);
+			$this->dispatchCheckoutEvent(SyliusCheckoutEvents::SHIPPING_PRE_COMPLETE, $order);
 
-        // TODO
-//        if ($form->handleRequest($request)->isValid()) {
-//            $this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_SHIPPING_PRE_COMPLETE, $order);
-//
-//            $this->getManager()->persist($order);
-//            $this->getManager()->flush();
-//
-//            $this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_SHIPPING_COMPLETE, $order);
-//
-//            return $this->complete();
-//        }
+			$this->getManager()->persist($order);
+			$this->getManager()->flush();
 
-        return $this->renderStep($context, $order, $formAddressing, $formShipping,$formPayment);
-    }
+			$this->dispatchCheckoutEvent(SyliusCheckoutEvents::ADDRESSING_COMPLETE, $order);
+			$this->dispatchCheckoutEvent(SyliusCheckoutEvents::PAYMENT_COMPLETE, $order);
+			$this->dispatchCheckoutEvent(SyliusCheckoutEvents::SHIPPING_COMPLETE, $order);
 
-    protected function renderStep(ProcessContextInterface $context, OrderInterface $order, FormInterface $formAddressing, FormInterface $formShipping, FormInterface $formPayment)
-    {
-        return $this->render($this->container->getParameter(sprintf('sylius.checkout.step.%s.template', $this->getName())), array(
-            'order'   => $order,
-            'formAddressing'    => $formAddressing->createView(),
-            'formShipping'    => $formShipping->createView(),
-            'formPayment'    => $formPayment->createView(),
-            'context' => $context
-        ));
-    }
+			return $this->complete();
+		}
 
-    protected function createCheckoutAddressingForm(OrderInterface $order, UserInterface $user = null)
-    {
-        return $this->createForm('sylius_checkout_addressing', $order, array('user' => $user));
-    }
+		return $this->renderStep($context, $order, $formAddressing, $formPayment, $formShipping);
+	}
 
-    protected function createCheckoutShippingForm(OrderInterface $order)
-    {
-        $this->zones = $this->getZoneMatcher()->matchAll($order->getShippingAddress());
+	protected function renderStep(ProcessContextInterface $context, OrderInterface $order, FormInterface $formAddressing, FormInterface $formPayment, FormInterface $formShipping = null)
+	{
+		return $this->render($this->container->getParameter(sprintf('sylius.checkout.step.%s.template', $this->getName())), array(
+			'order' => $order,
+			'formAddressing' => $formAddressing->createView(),
+			'formPayment' => $formPayment->createView(),
+			'formShipping' => $formShipping ? $formShipping->createView() : $formShipping,
+			'context' => $context
+		));
+	}
 
-        if (empty($this->zones)) {
-            $this->get('session')->getFlashBag()->add('error', 'sylius.checkout.shipping.error');
-        }
+	protected function createCheckoutAddressingForm(OrderInterface $order, UserInterface $user = null)
+	{
+		return $this->createForm('sylius_checkout_addressing', $order, array('user' => $user));
+	}
 
-        return $this->createForm('sylius_checkout_shipping', $order, array(
-          'criteria' => array(
-            'zone' => !empty($this->zones) ? array_map(function ($zone) {
-                return $zone->getId();
-            }, $this->zones) : null,
-            'enabled' => true,
-          )
-        ));
-    }
+	protected function createCheckoutShippingForm(OrderInterface $order)
+	{
+		$this->zones = $this->getZoneMatcher()->matchAll($order->getShippingAddress());
 
-    protected function createCheckoutPaymentForm(OrderInterface $order)
-    {
-        return $this->createForm('sylius_checkout_payment', $order, array('order_items' => $order->getItems()));
-    }
+		if (empty($this->zones)) {
+			$this->get('session')->getFlashBag()->add('error', 'sylius.checkout.shipping.error');
+		}
+
+		return $this->createForm('sylius_checkout_shipping', $order, array(
+			'criteria' => array(
+				'zone' => !empty($this->zones) ? array_map(function ($zone) {
+					return $zone->getId();
+				}, $this->zones) : null,
+				'enabled' => true,
+			)
+		));
+	}
+
+	protected function createCheckoutPaymentForm(OrderInterface $order)
+	{
+		return $this->createForm('sylius_checkout_payment', $order, array('order_items' => $order->getItems()));
+	}
 }
