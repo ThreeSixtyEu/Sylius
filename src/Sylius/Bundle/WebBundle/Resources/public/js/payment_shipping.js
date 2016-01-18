e553b5d4e3aed49e98440a86fc733da28ac5c7b7
David Alexa <d.alexa@3sixty.eu>
2016-01-18 14:57:21 +0100

    preneseni sablon pro payment_shipping step mezi obecne

---
+++ src/Sylius/Bundle/WebBundle/Resources/public/js/payment_shipping.js
@@ -0,0 +1,20 @@
+/**
+ * Created by dalexa on 15.12.15.
+ */
+
+$(document).ready(function() {
+	$("#shippings").on('change, click', 'input[type="radio"]', function() {
+		var $addressing = $("#addressing");
+		var requiredIds = $("#shippings").data('shippingsRequiresAddressing');
+		var id = parseInt($(this).val(), 10);
+
+		if (requiredIds.ids.indexOf(id) !== -1) {
+			$addressing.removeClass('js-hidden');
+		} else {
+			$addressing.addClass('js-hidden');
+		}
+	}).on('change', '#sylius_checkout_shipping_country', function() {
+		$(this).parents('form').submit();
+	});
+
+});
