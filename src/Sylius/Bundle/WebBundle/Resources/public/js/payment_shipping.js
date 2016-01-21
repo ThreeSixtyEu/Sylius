9f765fce45d86958d6d8316ec76f969103aa97aa
David Alexa <d.alexa@3sixty.eu>
2016-01-21 19:43:10 +0100

    ajax loading of country change in payment shipping step of checkout

--- src/Sylius/Bundle/WebBundle/Resources/public/js/payment_shipping.js
+++ src/Sylius/Bundle/WebBundle/Resources/public/js/payment_shipping.js
@@ -3,7 +3,7 @@
  */
 
 $(document).ready(function() {
-	$("#shippings").on('change, click', 'input[type="radio"]', function() {
+	$("body").on('change, click', '#shippings input[type="radio"]', function() {
 		var $addressing = $("#addressing");
 		var requiredIds = $("#shippings").data('shippingsRequiresAddressing');
 		var id = parseInt($(this).val(), 10);
@@ -13,13 +13,37 @@ $(document).ready(function() {
 		} else {
 			$addressing.addClass('js-hidden');
 		}
-	}).on('change', '#sylius_checkout_shipping_country_specific_country', function() {
+	}).on('change', '#sylius_checkout_shipping_country_specific_country, #sylius_checkout_addressing_shippingAddress_country', function() {
 		var $form = $(this).parents('form');
-		$form.prepend($("<input/>").attr('type', 'hidden').attr('name', 'doNotForward').val(true));
-		$form.submit();
+		var $input = $form.prepend($("<input/>").attr('type', 'hidden').attr('name', 'doNotForward').val(true));
+		$form.prepend($("<div />").addClass('ajax-loader'));
+		setTimeout(function() {
+			$.ajax({
+				url: $form.attr('action'),
+				method: 'POST',
+				data: $form.serialize(),
+				success: function(data) {
+					var $content = $(data).find("#payment_shipping_form");
+					if ($content.length) {
+						$form.html($content.html());
+						checkFirstShipping();
+					} else {
+						$form.find('.ajax-loader').remove();
+						$input.remove();
+					}
+				},
+				error: function() {
+					$form.find('.ajax-loader').remove();
+					$input.remove();
+				}
+			});
+		}, 50);
 	});
 
-	if (!$("#shippings input[type='radio']:checked").length) {
-		$("#shippings input[type='radio']").first().click();
+	function checkFirstShipping() {
+		if (!$("#shippings input[type='radio']:checked").length) {
+			$("#shippings input[type='radio']").first().click();
+		}
 	}
+	checkFirstShipping();
 });
